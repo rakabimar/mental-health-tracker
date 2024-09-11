d69faa65c8f1ec23ab68edd46d9c524b9660b590
rakabimar <rakabimaghaniendra22@gmail.com>
2024-09-11 16:00:16 +0700

    Fix views.py

--- main/views.py
+++ main/views.py
@@ -35,7 +35,7 @@ def show_json(request):
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
-    data = MoodEntry.objects.filter(id=id)
+    data = MoodEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json_by_id(request, id):
